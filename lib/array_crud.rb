51d8b07b93b2092e05453018a375d377da5fa78d
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-08 15:53:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ rem = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+rem = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
